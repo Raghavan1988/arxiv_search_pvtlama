ff6b7519f6bd09739cd159182adc49d7af125574
Raghavan <raghavan.mit@gmail.com>
2023-09-24 14:18:00 -0700

    Update index_documents_copy.py

--- index_documents_copy.py
+++ index_documents_copy.py
@@ -15,7 +15,7 @@ with open("/home/raghavan/arxiv-metadata-oai-snapshot.json", "r") as f:
         data.append(json.loads(line))
         counter += 1
         ### index only 1000000 documents
-        if (counter > 200000):
+        if (counter > 500000):
             break
         
 counter = 0
@@ -53,36 +53,97 @@ print ("number of documents" + str(len(documents)))
 retriever = BM25Retriever(document_store=document_store)
 
 from haystack.nodes.prompt import PromptNode
-pn = PromptNode("gpt-3.5-turbo", api_key="sk1",model_kwargs={"stream":False})
+pn = PromptNode("gpt-3.5-turbo", api_key="sk-123",model_kwargs={"stream":False})
 pipeline = DocumentSearchPipeline(retriever)
 
 while True:
     ## input user query
     query = "gravity"
     query = input("Enter query:")
-    raw_query = query
+    raw_query = query.strip()
+    query = query.strip()
     if query == "exit":
-        #print(results["documents"])
+        break
 
+    ## ASK llama to generate 2 different queries to optimize for recall
+    prompt = 'rewrite the QUERY: "' + query + '" in EXACTLY 3 different ways to optimize for the recall WITHOUT STOP WORDS. Output a SINGLE JSONARRAY of STRINGs with the KEY QUERY'
+
+
+    response = pn.prompt(prompt)
+    response_json = json.loads(response[0])
+    Q1 = response_json['QUERY'][0]
+    Q2 = response_json['QUERY'][1]
+    Q3 = response_json['QUERY'][2]
+
+    KP = []
+    ## Ask llama to generate 2 key phrases for the query
+    if (len(query.split()) >= 6):
+        key_phrases = 'EXTRACT the 2 key phrases of the QUERY: "' + query + '" Output a SINGLE STRING JSONARRAY of the keyphrases with the KEY KP'
+        response = pn.prompt(key_phrases)
+        response_json = json.loads(response[0])
+        #print(response)
+        try:
+            KP = response_json['KP']
+        except:
+            KP = []
+
+   
+
+    queries = [raw_query,Q1,Q2,Q3]
+    if (len(KP) > 0):
+        queries.append(KP[0])
+        queries.append(KP[1])
+
+    print ("Number of backend queries:" + str(len(queries)))
+    print(queries)
+    documents_to_evaluate = []
+    titles = set()
+    round  = 0
+    for query in queries:
+        results = pipeline.run(query, params={"Retriever": {"top_k":5}})
+        print ("Round : " + str(round))
+        round += 1
+        print("Backend query:" + query)
+        
+        for document in results["documents"]:
+            if document.meta["id"] not in titles:
+                documents_to_evaluate.append(document)
+                titles.add(document.meta["id"])
+        
+
+   
+    w = open("prompt_text","w")
+    text = " Take a deep breadth and do the following task. You are a Search Engine Ranker. You have QUERY and SEARCH RESULTS. Rank all the RESULT IDS in SEARCH RESULTS based on CONTENT's relevance to the QUERY. QUERY: " + raw_query.strip() + "\n"
+    text += "SEARCH RESULTS: \n"
+    counter = 0
+
+
+    #####
+    print ("Number of documents to evaluate:" + str(len(documents_to_evaluate)))
+    for result in documents_to_evaluate:
         text += "RESULT ID :" + result.meta["id"] + "\n"
         w.write("RESULT ID :" + result.meta["id"] + "\n")
 
-        text += result.content + "\n"
-        w.write(result.content + "\n")
+        text += "CONTENT:" + result.content.strip() + "\n"
+        w.write("CONTENT:" + result.content + "\n")
         w.write("---------------------------\n")
    
-    text += "Based on all the results above, generate a PARAGRAPH OF SUMMARY summarizing of the results for the QUERY: " + raw_query
-    w.write("Based on all the results above, generate a PARAGRAPH OF SUMMARY summarizing of the results for the QUERY: " + raw_query)
+    text += "Based on all the results above, generate a DETAILED SUMMARY providing multiple POINTS OF VIEW summarizing of the results and provide a ANSWER and TAKEAWAY for the QUERY: " + raw_query.strip()
+    #w.write("Based on all the results above, generate a PARAGRAPH OF SUMMARY summarizing of the results for the QUERY: " + raw_query)
+
+
+    #text += " Rank all the RESULT IDs based on the relevance for the QUERY " + raw_query + " AND CONTENT. Output the SORTED ORDER OF the RESULTS in a SINGLE JSONARRAY of RANKED RESULT IDS based on relevance to the QUERY with the KEY ORDERED"
+    #w.write(" Rank all the RESULT ID based on the relevance for the QUERY and output the SORTED ORDER OF the RESULTS in a SINGLE JSONARRAY of IDS with the KEY RESULTS")
     w.close()
-    #text += " Rank all the RESULT ID based on the relevance for the QUERY and output the SORTED ORDER OF the RESULTS in a SINGLE JSONARRAY of IDS with the KEY RESULTS\n"
-    #w.write(" Rank all the RESULT ID based on the relevance for the QUERY and output the SORTED ORDER OF the RESULTS in a SINGLE JSONARRAY of IDS with the KEY RESULTS\n")
+    
     print("wrote the prompt to the file: " + raw_query)
 
-    
-
-
     try:
+        pn = PromptNode("gpt-3.5-turbo", api_key="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",model_kwargs={"stream":False})
+
         output= pn.prompt(text)
+        print("Model Response:", output)  # Print the model's response
+
         print(output[0])
     except Exception as e:
         print(e)
